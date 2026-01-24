83441efd1f4164b11579c33542bd52e4d0b6a549
Ana Elisa <anasiani254@hotmail.com>
2026-01-24 20:17:15 -0300

    Comçando exploração da base, e criando um dataset menor, com glossário para entendimento dos dados, e aproveitamento melhor de informações conhecidas, focando na qualidade dos dados que serão explorados

--- notebooks/notebook.py
+++ notebooks/notebook.py
@@ -10,7 +10,76 @@ kaggle.api.authenticate()
 
 df = pd.read_csv('notebooks/Xtest.csv', sep=',')
 
-print("Informações: ", df.info) # infos tipo de dados, se a muitos dados ausentes e etc
+# Visualizar as primeiras linhas
+'''print("Primeiras linhas do dataset:")
+print(df.head())
 print("\n")
 
-print("Valores vazios", df.isnull().sum())
+# Informações gerais (tipos e dados nulos)
+print("Informações do dataset:")
+df.info()
+print("\n")
+
+# Verificar valores ausentes por coluna
+print("Valores vazios por coluna:")
+print(df.isnull().sum())
+print("\n")
+
+print("Tamanho do dataset:")
+print(df.shape)
+print("\n")
+
+print("Tipos de dados das colunas na Tab:")
+print(df.dtypes.value_counts())
+print("\n")
+
+print("Identificar colunas com 1 valor, poucos, etc:")
+print(df.nunique().sort_values())
+print("\n")'''
+
+'''print("Nomes das coluns:")
+for col in df.columns:
+    print(col)
+print("\n")'''
+
+colunas_iniciais = [
+    "LotFrontage", "LotArea", "GrLivArea", "TotalBsmtSF",
+    "OverallQual", "OverallCond", "Total_Home_Quality",
+    "YearBuilt", "Age", "Renovate",
+    "FullBath", "HalfBath", "Total_Bathrooms",
+    "BedroomAbvGr", "KitchenAbvGr", "TotRmsAbvGrd",
+    "GarageCars", "GarageArea",
+    "Fireplaces", "WoodDeckSF", "OpenPorchSF", "PoolArea",
+    "SqFtPerRoom", "HighQualSF"
+]
+
+df_base = df[colunas_iniciais]
+
+df_base = df_base.rename(columns={
+    "LotFrontage": "Largura da frente do terreno",
+    "LotArea": "Área total do terreno",
+    "GrLivArea": "Área construída acima do solo",
+    "TotalBsmtSF": "Área total do porão",
+    "OverallQual": "Qualidade geral do imóvel",
+    "OverallCond": "Condição geral do imóvel",
+    "Total_Home_Quality": "Qualidade total combinada",
+    "YearBuilt": "Ano de construção",
+    "Age": "Idade do imóvel",
+    "Renovate": "Indica se houve reforma",
+    "FullBath": "Banheiros completos",
+    "HalfBath": "Lavabos",
+    "Total_Bathrooms": "Total de banheiros",
+    "BedroomAbvGr": "Número de quartos",
+    "KitchenAbvGr": "Número de cozinhas",
+    "TotRmsAbvGrd": "Total de cômodos",
+    "GarageCars": "Capacidade da garagem",
+    "GarageArea": "Área da garagem",
+    "Fireplaces": "Número de lareiras",
+    "WoodDeckSF": "Área de deck",
+    "OpenPorchSF": "Área de varanda aberta",
+    "PoolArea": "Área da piscina",
+    "SqFtPerRoom": "Área média por cômodo",
+    "HighQualSF": "Área de alta qualidade"
+})
+
+print(df_base.head())
